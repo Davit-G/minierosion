c0dd14cf29e9c77a495251229af91129954e38bd
Davit Gogiberidze <razormayhem4z@gmail.com>
2023-06-07 11:22:51 +1000

    first commit

---
+++ PluginEditor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+
+/**
+ * 
+ * 
+ * This class is not being used
+*/
+
+//==============================================================================
+class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    AudioPluginAudioProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
---
+++ PluginProcessor.cpp
@@ -0,0 +1,232 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessor::AudioPluginAudioProcessor()
+     : AudioProcessor (BusesProperties()
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                       .withInput  ("Sidechain",  juce::AudioChannelSet::stereo(), true)
+                       .withInput  ("Sidechain 2",  juce::AudioChannelSet::stereo(), true)
+                       .withInput  ("Sidechain 3",  juce::AudioChannelSet::stereo(), true)
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                       ),
+    treeState(*this, nullptr, "savedParams", createParameterLayout())
+{
+    treeState.state = juce::ValueTree("savedParams");
+
+    erosionAmount = dynamic_cast<juce::AudioParameterFloat *>(treeState.getParameter("amount"));
+    jassert(erosionAmount);
+    erosionFrequency = dynamic_cast<juce::AudioParameterFloat *>(treeState.getParameter("freq"));
+    jassert(erosionFrequency);
+    erosionQ = dynamic_cast<juce::AudioParameterFloat *>(treeState.getParameter("q"));
+    jassert(erosionQ);
+    useSidechain = dynamic_cast<juce::AudioParameterBool *>(treeState.getParameter("useSidechain"));
+    jassert(useSidechain);
+
+    erosionDSP = std::make_unique<Erosion>(erosionAmount, erosionFrequency, erosionQ);
+
+    
+}
+
+AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
+{
+}
+
+
+
+juce::AudioProcessorValueTreeState::ParameterLayout AudioPluginAudioProcessor::createParameterLayout()
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout params;
+
+    params.add(std::make_unique<juce::AudioParameterFloat>("amount", "amount", 0.0f, 100.0f, 0.f));
+    params.add(std::make_unique<juce::AudioParameterFloat>("freq", "freq", 0.0f, 22000.0f, 400.f));
+    params.add(std::make_unique<juce::AudioParameterFloat>("q", "q", 0.0f, 3.0f, 1.f));
+    params.add(std::make_unique<juce::AudioParameterBool>("useSidechain", "useSidechain", false));
+
+    return params;
+}
+
+//==============================================================================
+const juce::String AudioPluginAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool AudioPluginAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool AudioPluginAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool AudioPluginAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double AudioPluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int AudioPluginAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int AudioPluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void AudioPluginAudioProcessor::setCurrentProgram (int index)
+{
+    juce::ignoreUnused (index);
+}
+
+const juce::String AudioPluginAudioProcessor::getProgramName (int index)
+{
+    juce::ignoreUnused (index);
+    return {};
+}
+
+void AudioPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+    juce::ignoreUnused (index, newName);
+}
+
+//==============================================================================
+void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    juce::ignoreUnused (sampleRate, samplesPerBlock);
+
+    erosionDSP->prepareToPlay(sampleRate, samplesPerBlock);
+
+    this->sampleRate = sampleRate;
+}
+
+void AudioPluginAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+
+void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
+                                              juce::MidiBuffer& midiMessages)
+{
+    juce::ignoreUnused (midiMessages);
+
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    auto outBuffer = getBusBuffer(buffer, true, 0);
+    auto busBuffer = getBusBuffer(buffer, true, 1);
+
+    juce::dsp::AudioBlock<float> busBlock(busBuffer);
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+
+    juce::dsp::AudioBlock<float> block(buffer);
+
+    if (useSidechain->get() && getNumInputChannels() > 2) {
+        erosionDSP->processSidechainedBlock(block, busBlock, sampleRate);
+    } else {
+        erosionDSP->processBlock(block, sampleRate);
+    }
+}
+
+//==============================================================================
+bool AudioPluginAudioProcessor::hasEditor() const
+{
+    return false; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
+{
+    return nullptr; //new AudioPluginAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void AudioPluginAudioProcessor::getStateInformation(juce::MemoryBlock &destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    std::unique_ptr<juce::XmlElement> xml(treeState.copyState().createXml());
+    copyXmlToBinary(*xml, destData);
+}
+
+void AudioPluginAudioProcessor::setStateInformation(const void *data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    if (xmlState.get() != nullptr)
+        if (xmlState->hasTagName(treeState.state.getType()))
+            // treeState.replaceState(juce::ValueTree::fromXml(*xmlState));
+            treeState.state = juce::ValueTree::fromXml(*xmlState);
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new AudioPluginAudioProcessor();
+}
